8c449073245fb3e4ae7756932d76a72e34bc4e3c
lik2hardik <lik2hardik@gmail.com>
2026-07-16 17:47:15 +0000

    feat: Add igestable file class as input type to ingestor

--- backend/ingestors/ingestor.py
+++ backend/ingestors/ingestor.py
@@ -1,13 +1,32 @@
-from abc import  abstractmethod
+from abc import abstractmethod,ABC
+import io
+
 class Ingestor:
-    def __init(self,type = None,accepted_format = None,name = "default"):
+    def __init(self, type=None, accepted_format=None, name="default"):
         self.name = name
         self.type = type
         self.format = accepted_format
-    
+
     @abstractmethod
-    async def extract_text(self,media) -> str:
-        '''
+    async def extract_text(self, file) -> dict:
+        """
         Given the media object, extract the relevant text.
-        '''
+        """
         pass
+
+
+class IngestableFile():
+    def __init__(self, file_obj: io.IOBase):
+        """
+        Accepts any file-like object passed from the application.
+        """
+        self.file_obj = file_obj
+        self.file_name = getattr(file_obj, 'name', 'unknown_source')
+        self.extension = self.file_name.split('.')[-1].lower() if '.' in self.file_name else 'unknown'
+
+    def get_file(self):
+        return {
+           "file_name" : self.file_name,
+           "extension" : self.extension,
+           "file": self.file_obj
+        }
