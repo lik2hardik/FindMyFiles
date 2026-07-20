c6aedb70214307091435e82efb10dde4db62cc52
lik2hardik <lik2hardik@gmail.com>
2026-07-20 13:57:23 +0000

    refactor: Update extract_text method to return text and metadata as separate values

--- backend/ingestors/text_ingestor.py
+++ backend/ingestors/text_ingestor.py
@@ -17,6 +17,5 @@ class TextIngestor(Ingestor):
         file.file_obj.seek(0)
         data = file.file_obj.read()
         if isinstance(data, bytes):
-            return data.decode("utf-8", errors="ignore")
+            data = data.decode("utf-8", errors="ignore")
         return data, self.extract_metadata(file)
-
