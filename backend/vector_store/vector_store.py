caf0e45dd167d0284515443af5892fff9fa111b4
lik2hardik <lik2hardik@gmail.com>
2026-07-20 13:46:35 +0000

    feat: Update VectorStore methods to be asynchronous and add default get_md5 for unique chunk IDs

--- backend/vector_store/vector_store.py
+++ backend/vector_store/vector_store.py
@@ -1,4 +1,5 @@
 from abc import abstractmethod, ABC
+import hashlib
 
 
 class VectorStore(ABC):
@@ -6,9 +7,14 @@ class VectorStore(ABC):
         self.path = path
 
     @abstractmethod
-    def add(self, chunks: list[str], metadata: dict = None):
+    async def add(self, chunks: list[str], metadata: dict = None):
         "store chunks in the vector database."
 
     @abstractmethod
-    def get(self, query: str, k=10, constraints: dict = None):
+    async def get(self, query: str, k=10, constraints: dict = None):
         "return the relevant chunks."
+
+    @staticmethod
+    async def get_md5(chunks: list[str]) -> list[str]:
+        "Get the md5 for chunk to serve as unique ids for vector store."
+        return [hashlib.md5(chunk.encode("utf-8")).hexdigest() for chunk in chunks]
