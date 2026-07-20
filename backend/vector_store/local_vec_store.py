87bbcf5bec690d92fdc0a8e16929a029b0381f9b
lik2hardik <lik2hardik@gmail.com>
2026-07-20 14:00:23 +0000

    feat: Implement Chroma_DB_Vector_Store for managing local vector data with asynchronous add and query methods

---
+++ backend/vector_store/local_vec_store.py
@@ -0,0 +1,25 @@
+from vector_store.vector_store import VectorStore
+from chromadb import PersistentClient
+from ingestors.ingestor import Metadata
+
+
+class Chroma_DB_Vector_Store(VectorStore):
+    def __init__(self, path="backend/data/vecstore/"):
+        super().__init__(path)
+
+        self.client = PersistentClient(self.path)
+        self.collection = self.client.get_or_create_collection(
+            name="my-collection", metadata={"description": "vector-store for data"}
+        )
+
+    async def add(self, chunks, metadatas: list[Metadata]):
+
+        self.collection.add(
+            ids=await self.get_md5(chunks), documents=chunks, metadatas=metadatas
+        )
+
+    async def get(self, query, k=10, constraints=None):
+        self.collection.query(
+            query_texts=[query],
+            n_results=k
+        )
