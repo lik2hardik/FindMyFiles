0f3be9e2eb5f590f27a22002b148a7c24e5fafc3
lik2hardik <lik2hardik@gmail.com>
2026-07-20 13:43:10 +0000

    feat: Add Metadata class for file ingestion and add extract_metadata method as default

--- backend/ingestors/ingestor.py
+++ backend/ingestors/ingestor.py
@@ -1,8 +1,16 @@
 from abc import abstractmethod, ABC
 from backend.filestore.filestore import IngestableFile
+from pydantic import BaseModel,Field
+from datetime import datetime, timezone
 
+class Metadata(BaseModel):
+    file_name: str 
+    type: str
+    extension: str
+    created_at_ts: float = Field(default_factory=lambda: datetime.now(timezone.utc).timestamp())
 
 class Ingestor(ABC):
+    "Class to Ingest file into chunks and metadata."
     def __init__(self, type=None, accepted_format=None, name="default"):
         self.name = name
         self.type = type
@@ -14,3 +22,10 @@ class Ingestor(ABC):
         Given the media object, extract the relevant text.
         """
         pass
+    
+    def extract_metadata(self, file: IngestableFile) -> Metadata:
+        return Metadata(
+            file_name= file.file_name,
+            type = self.type,
+            extension = file.extension
+        )
