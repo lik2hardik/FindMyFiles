22e90eb43decb7e77c7097f90155499ef61bbcbb
lik2hardik <lik2hardik@gmail.com>
2026-08-03 08:57:56 +0000

    fix: Refactor ChromaDBVectorStore to initialize client and collection lazily

--- backend/vector_store/local_vec_store.py
+++ backend/vector_store/local_vec_store.py
@@ -6,16 +6,21 @@ from backend.ingestors.ingestor import Metadata
 class ChromaDBVectorStore(VectorStore):
     def __init__(self, path="backend/data/vecstore/"):
         super().__init__(path)
+        self._client = None
+        self._collection = None
 
-        self.client = PersistentClient(self.path)
-        self.collection = self.client.get_or_create_collection(
-            name="my-collection", metadata={"description": "vector-store for data"}
-        )
+    @property
+    def collection(self):
+        if self._collection is None:
+            self._client = PersistentClient(path=self.path)
+            self._collection = self._client.get_or_create_collection(
+                name="my-collection",
+                metadata={"description": "vector-store for data"},
+            )
+        return self._collection
 
     def add(self, chunks, metadatas: list[Metadata]):
-
         metadata_dicts = [m.model_dump() for m in metadatas]
-
         self.collection.add(
             ids=self.get_md5(chunks), documents=chunks, metadatas=metadata_dicts
         )
