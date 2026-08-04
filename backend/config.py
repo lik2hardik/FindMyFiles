29f0e3525437ad62b91bc86d7feaca9354ce7db0
lik2hardik <lik2hardik@gmail.com>
2026-08-04 16:17:04 +0000

    feat: Update config and tasks to integrate ImageOCRIngestor with error handling for ingestion

--- backend/config.py
+++ backend/config.py
@@ -3,11 +3,20 @@
 
 from backend.filestore.local_filestore import LocalSQLiteFileStore
 from backend.ingestors.text_ingestor import TextIngestor
+from backend.ingestors.image_ingestor import ImageOCRIngestor
 from backend.vector_store.local_vec_store import ChromaDBVectorStore
 from backend.chunker.recursive_chunker import RecursiveChunker
+import dotenv
+
 import os
 
+GROQ_KEY = dotenv.dotenv_values("GROQ_KEY")
+
 TEXT_INGESTORS = TextIngestor(accepted_format=["txt", "md"])
+TEXT_INGESTORS = ImageOCRIngestor(accepted_format=["txt", "md"],
+                                  GROQ_KEY=GROQ_KEY,
+                                  use_api=True)
+
 CHUNKER = RecursiveChunker(chunk_size=512, overlap=64)
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
--- backend/tasks.py
+++ backend/tasks.py
@@ -1,6 +1,6 @@
 import time
 from backend.celery_app import celery_app
-from backend.ingestors.ingestor import Ingestor
+from backend.ingestors.ingestor import Ingestor,IngestFailed
 from backend.config import CHUNKER, FILE_STORE, VECTOR_STORE
 
 
@@ -21,13 +21,23 @@ def process_ingest_file(file_id) -> str:
 
     print(f"Starting processing for {file.file_name}...")
 
-    ingestor: Ingestor = Ingestor.ingestor_map.get(file.extension, None)
-    text, metadata = ingestor.extract_text(file)
-    print(f"Ingestion complete for {file.file_name}...")
-    text_chunks = CHUNKER.split_text(text)
-    print(f"Chunking complete for {file.file_name}...")
-    VECTOR_STORE.add(text_chunks, [metadata] * len(text_chunks))
-    print(f"Vector embeddings complete for {file.file_name}...")
-    print(f" complete for {file.file_name}...")
+    try:
+        ingestor: Ingestor = Ingestor.ingestor_map.get(file.extension, None)
+        text, metadata = ingestor.extract_text(file)
+        print(f"Ingestion complete for {file.file_name}...")
+        text_chunks = CHUNKER.split_text(text)
+        print(f"Chunking complete for {file.file_name}...")
+        VECTOR_STORE.add(text_chunks, [metadata] * len(text_chunks))
+        print(f"Vector embeddings complete for {file.file_name}...")
+        print(f" complete for {file.file_name}...")
 
-    print(f"Finished processing {file.file_name}!")
+        print(f"Finished processing {file.file_name}!")
+
+        return 0
+
+    except IngestFailed as e:
+        return f"Ingestion Error: {e}"
+    except:
+        return "Error"
+
+
